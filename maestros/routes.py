1f57245b294669bebaddb3ce251621ac3a5d7942
IDGS-803-23001066 <sanchezperezgilberto9@gmail.com>
2026-03-09 20:43:34 -0600

    Maestros

---
+++ maestros/routes.py
@@ -0,0 +1,129 @@
+from flask import redirect, render_template, request, url_for
+from models import Maestros, db
+from forms import MaestroForm
+from maestros import maestros
+
+
+
+
+@maestros.route('/perfil/<nombre>')
+def perfil(nombre):
+    return f"Perfil de {nombre}"
+
+@maestros.route("/Maestros", methods=['GET','POST'])
+def lista_maestros():
+
+    create_form = MaestroForm(request.form)
+    maestros = Maestros.query.all()   # ← CONSULTA A LA BD
+
+    if request.method == 'POST' and create_form.validate():
+
+        maestro = Maestros(
+            matricula=create_form.matricula.data,
+            nombre=create_form.nombre.data,
+            apellidos=create_form.apellidos.data,
+            especialidad=create_form.especialidad.data,
+            email=create_form.email.data
+        )
+
+        db.session.add(maestro)
+        db.session.commit()
+
+        return redirect(url_for('maestros.lista_maestros'))
+
+    return render_template(
+        "listadoMaestros.html",
+        form=create_form,
+        maestros=maestros   # ← SE ENVÍAN AL HTML
+    )
+
+@maestros.route("/detalles_maestro")
+def detalles_maestro():
+
+    matricula = request.args.get('matricula')
+    maestro = Maestros.query.get(matricula)
+
+    return render_template(
+        "detallesMaestros.html",
+        nombre=maestro.nombre,
+        apellidos=maestro.apellidos,
+        especialidad=maestro.especialidad,
+        email=maestro.email
+    )
+@maestros.route("/modificar_maestro", methods=['GET','POST'])
+def modificar_maestro():
+
+    create_form = MaestroForm(request.form)
+
+    if request.method == 'GET':
+        matricula = request.args.get('matricula')
+        maestro = Maestros.query.get(matricula)
+
+        create_form.matricula.data = maestro.matricula
+        create_form.nombre.data = maestro.nombre
+        create_form.apellidos.data = maestro.apellidos
+        create_form.especialidad.data = maestro.especialidad
+        create_form.email.data = maestro.email
+
+    if request.method == 'POST' and create_form.validate():
+        matricula = create_form.matricula.data
+        maestro = Maestros.query.get(matricula)
+
+        maestro.nombre = create_form.nombre.data
+        maestro.apellidos = create_form.apellidos.data
+        maestro.especialidad = create_form.especialidad.data
+        maestro.email = create_form.email.data
+
+        db.session.commit()
+
+        return redirect(url_for('maestros.lista_maestros'))
+
+    return render_template("modificarMaestros.html", form=create_form)
+
+@maestros.route("/eliminar_maestro", methods=['GET','POST'])
+def eliminar_maestro():
+
+    create_form = MaestroForm(request.form)
+
+    if request.method == 'GET':
+        matricula = request.args.get('matricula')
+        maestro = Maestros.query.get(matricula)
+
+        create_form.matricula.data = maestro.matricula
+        create_form.nombre.data = maestro.nombre
+        create_form.apellidos.data = maestro.apellidos
+        create_form.especialidad.data = maestro.especialidad
+        create_form.email.data = maestro.email
+
+    if request.method == 'POST':
+        matricula = create_form.matricula.data
+        maestro = Maestros.query.get(matricula)
+
+        db.session.delete(maestro)
+        db.session.commit()
+
+        return redirect(url_for('maestros.lista_maestros'))
+
+    return render_template("eliminarMaestros.html", form=create_form)
+
+@maestros.route("/registrar_maestro", methods=['GET','POST'])
+def registrar_maestro():
+
+    create_form = MaestroForm(request.form)
+
+    if request.method == 'POST' and create_form.validate():
+
+        maestro = Maestros(
+            matricula=create_form.matricula.data,
+            nombre=create_form.nombre.data,
+            apellidos=create_form.apellidos.data,
+            especialidad=create_form.especialidad.data,
+            email=create_form.email.data
+        )
+
+        db.session.add(maestro)
+        db.session.commit()
+
+        return redirect(url_for('maestros.lista_maestros'))
+
+    return render_template("Maestros.html", form=create_form)
